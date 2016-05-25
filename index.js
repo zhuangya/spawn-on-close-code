93549d4a82557c86ca9c3312e21bf4561c7fccaa
Ya Zhuang <zhuangya@gmail.com>
2016-05-25 10:53:25 +0800

    add: different spawn methods

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 'use strict';
 
-var spawn = require('child_process').spawn;
-
-module.exports = function (action, callback) {;
+module.exports = function (spawn, action, callback) {;
   var grep = spawn('grep', ['ssh']);
   action(grep);
   grep.on('close', function (code, signal) {
--- test.js
+++ test.js
@@ -4,11 +4,22 @@ var assert = require('assert');
 
 var grepChild = require('./');
 
-['SIGTERM', 'SIGHUP', 'SIGKILL'].forEach(function (sig) {
-  grepChild(function (p) {
-    p.kill(sig);
-  }, function (code, signal) {
-    assert.equal(code, null);
-    assert.equal(signal, sig);
+var originalSpawn = require('child_process').spawn;
+var crossSpawn = require('cross-spawn');
+var crossSpawnAsync = require('cross-spawn-async');
+
+[
+  { name: 'originalSpawn', fn: originalSpawn },
+  { name: 'crossSpawn', fn: crossSpawn},
+  { name: 'crossSpawnAsync', fn: crossSpawnAsync}
+].forEach(function (spawn) {
+  ['SIGTERM', 'SIGHUP', 'SIGKILL'].forEach(function (sig) {
+    console.log(spawn.name, sig);
+    grepChild(spawn.fn, function (p) {
+      p.kill(sig);
+    }, function (code, signal) {
+      assert.equal(code, null);
+      assert.equal(signal, sig);
+    });
   });
 });
