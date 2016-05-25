ba20fdff0565b667e98f7dec7843a1ef75ec4f20
Ya Zhuang <zhuangya@gmail.com>
2016-05-25 11:27:16 +0800

    add: test and coverage by tap

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var assert = require('assert');
+var tap = require('tap');
 
 var grepChild = require('./');
 
@@ -8,18 +8,28 @@ var originalSpawn = require('child_process').spawn;
 var crossSpawn = require('cross-spawn');
 var crossSpawnAsync = require('cross-spawn-async');
 
-[
+var spawns = [
   { name: 'originalSpawn', fn: originalSpawn },
   { name: 'crossSpawn', fn: crossSpawn},
   { name: 'crossSpawnAsync', fn: crossSpawnAsync}
-].forEach(function (spawn) {
-  ['SIGTERM', 'SIGHUP', 'SIGKILL'].forEach(function (sig) {
-    console.log(spawn.name, sig);
-    grepChild(spawn.fn, function (p) {
-      p.kill(sig);
-    }, function (code, signal) {
-      assert.equal(code, null);
-      assert.equal(signal, sig);
+];
+
+var signals = ['SIGTERM', 'SIGHUP', 'SIGKILL'];
+
+tap.test('spawn and signal', function (t) {
+  spawns.forEach(function (spawn, spawnIndex) {
+    signals.forEach(function (sig, sigIndex) {
+      grepChild(spawn.fn, function (p) {
+        p.kill(sig);
+      }, function (code, signal) {
+        t.equal(code, null);
+        t.equal(signal, sig);
+
+        if (spawnIndex === 0 && sigIndex === 0) {
+          t.end();
+        }
+      });
     });
   });
 });
+
